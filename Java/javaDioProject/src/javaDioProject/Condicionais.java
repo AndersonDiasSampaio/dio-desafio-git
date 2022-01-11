218c9b5c8fe3e63be95913be2681a7a91f8ef294
AndersonDiasSampaio <ads@ic.ufal.br>
2022-01-11 10:10:26 -0300

    java condicionais and operators

--- Java/javaDioProject/src/javaDioProject/Condicionais.java
+++ Java/javaDioProject/src/javaDioProject/Condicionais.java
@@ -1,14 +1,52 @@
 package javaDioProject;
-
+//Aula 1
 public class Condicionais {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int a, b, c;
-a=1;
-b=5;
-c=a+b;
-System.out.println(c);
+int i1=10, i2=20;
+float f1=4.5f, f2=3.5f;
+char c1='x', c2='y';
+String s1="Fulano", s2="Fulano";
+boolean b1=true, b2=false;
+if(i1==i2) {
+	System.out.println("igual");
+}else if(i1!=i2) {
+	System.out.println("Diferente");
+}else if(i1>i2) {
+	System.out.println("Maior");
+}else if(i1<=i2){
+	System.out.println("Menor ou igual");
+}
+if(f1==f2) {
+	System.out.println("igual");
+}else if(f1!=f2) {
+	System.out.println("Diferente");
+}else if(f1>f2) {
+	System.out.println("Maior");
+}else if(f1<=f2){
+	System.out.println("Menor ou igual");
+}
+if(c1==c2) {
+	System.out.println("igual");
+}else if(c1!=c2) {
+	System.out.println("Diferente");
+}else if(c1>c2) {
+	System.out.println("Maior");
+}else if(c1<=c2){
+	System.out.println("Menor ou igual");
+}
+if(s1==s2) {
+	System.out.println("igual");
+}else if(s1!=s2) {
+	System.out.println("Diferente");
+}
+if(b1==b2) {
+	System.out.println("igual");
+}else if(b1!=b2) {
+	System.out.println("Diferente");
+}
+
 	}
 
 }
