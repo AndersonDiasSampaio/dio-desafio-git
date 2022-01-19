7d6130791bfa15080cc526b453097262355a3956
AndersonDiasSampaio <ads@ic.ufal.br>
2022-01-19 11:09:38 -0300

    GENERIC CONCEPTS ON NO CLASS

--- Java/estruturaDadosDio/src/estruturaDadosDio/Main.java
+++ Java/estruturaDadosDio/src/estruturaDadosDio/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		No no1= new No("Conteudo No1");
-		No no2= new No("Conteudo No2");
+		No<String> no1= new No<String>("Conteudo No1");
+		No<String> no2= new No<String>("Conteudo No2");
 		no1.setProximoNo(no2);
-		No no3= new No("Conteudo No3");
+		No<String> no3= new No<String>("Conteudo No3");
 		no2.setProximoNo(no3);
 		System.out.println(no1);
 		System.out.println(no1.getProximoNo());
