7a5859e392ce928b15c72b4c9dd8c8dc29689dec
AndersonDiasSampaio <ads@ic.ufal.br>
2022-01-22 11:05:18 -0300

    list exerc for dio course

---
+++ Java/estruturaDadosDio/src/estruturaDadosDio/Lista.java
@@ -0,0 +1,19 @@
+package estruturaDadosDio;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Lista {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+List <Double> notas2= new LinkedList();
+notas2.add(1d);
+notas2.add(3d);
+notas2.add(4d);
+System.out.println(notas2.get(0));
+System.out.println(notas2.remove(0));
+System.out.println(notas2.toString());
+	}
+
+}
